c0a5fdba1c567804050e2dad8e91d3404dce83b8
amir-pasalic <amirpasalic144@gmail.com>
2021-03-23 20:17:04 +0100

    Assignment done !

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+
+class Person {
+  constructor(name, email, age) {
+    this._name = name;
+    this._email = email;
+    this._age = age;
+  }
+
+  getPerson() {
+    let personInfo = `Name: ${this._name}, Email: ${this._email}, Age: ${this._age}`;
+    return personInfo;
+  }
+}
+module.exports = Person;
+ 
